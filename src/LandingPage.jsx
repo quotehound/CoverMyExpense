4b6e7ecc9aabb52e78281437533bcd832c70ba39
David Verduzco <67717053+tangentdigitalagency@users.noreply.github.com>
2021-12-10 09:07:09 -0700

    buttonf Fix

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -219,11 +219,12 @@ class LandingPage extends Component {
       <div className="w-full lg:w-4/6 px-4">
         <div className="flex flex-wrap items-center justify-end">
           <ul className="w-full lg:w-auto inline-flex flex-wrap mb-4 lg:mb-0 md:mr-6 lg:mr-12">
-            <li className="mr-12 mb-2 md:mb-0"><a className="text-sm font-medium" href="https://www.quotehound.com/dont-sell-my-info" data-config-id="01_link1">Do Not Sell</a></li>
+            <li className="mr-12 mb-2 md:mb-0"><a className="text-sm font-medium" href="https://www.
+hound.com/dont-sell-my-info" data-config-id="01_link1">Do Not Sell</a></li>
             <li className="mr-12 mb-2 md:mb-0"><a className="text-sm font-medium"  href="https://www.quotehound.com/privacy-policy" data-config-id="01_link2">Privacy Policy</a></li>
             <li className="mr-12 mb-2 md:mb-0"><a className="text-sm font-medium"  href="https://www.quotehound.com/terms-conditions" data-config-id="01_link3">Terms & Conditions</a></li>
           </ul>
-          <a className="inline-block mr-auto lg:mr-0 py-4 px-8 text-sm text-white font-medium leafing-normal bg-blue-500 hover:bg-blue-300 hover:shadow-lg rounded" onClick={this.autoFocusClick} data-config-id="primary-action">Get Your No Quote Quote</a>
+          <a className="inline-block mr-auto lg:mr-0 py-4 px-8 text-sm text-white font-medium leafing-normal bg-blue-500 hover:bg-blue-300 hover:shadow-lg rounded" onClick={this.autoFocusClick} data-config-id="primary-action">Get Your No Obligation Quote</a>
         </div>
         <p className="mt-6 lg:hidden text-sm text-gray-500" data-config-id="copy">All rights reserved © Quotehound 2021</p>
       </div>
